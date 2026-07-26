fbf68bdefa69f13e1c5b2724a8d3823e03ead45e
ShahidAfzal <105744483+shahid-afzal@users.noreply.github.com>
2026-07-27 03:02:00 +0530

    implement job fetching and SQLlite storage

---
+++ src/database.py
@@ -0,0 +1,53 @@
+import sqlite3
+from datetime import datetime
+
+def connect_database():
+    connection = sqlite3.connect("jobs.db")
+    return connection
+
+def create_jobs_table(connection):
+    cursor = connection.cursor()
+    cursor.execute("""
+        CREATE TABLE IF NOT EXISTS jobs (
+            job_id  TEXT PRIMARY KEY,
+            title  TEXT,
+            company  TEXT,
+            location TEXT,
+            description  TEXT,
+            created  TEXT,
+            category  TEXT,
+            redirect_url  TEXT,
+            date_fetched  TEXT
+        )
+    """)
+    connection.commit()
+
+def insert_job(connection, job):
+    job_id = job["id"]
+    company = job["company"]["display_name"]
+    title  = job["title"]
+    location = job["location"]["display_name"]
+    description = job["description"]
+    created = job["created"]
+    category = job["category"]["label"]
+    redirect_url = job["redirect_url"]
+    date_fetched  = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+
+    cursor = connection.cursor()
+    cursor.execute("""
+        INSERT OR IGNORE INTO jobs (job_id, title, company, location, description, created, category, redirect_url, date_fetched)
+        VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)
+    """, (job_id, title, company, location, description, created, category, redirect_url, date_fetched))
+    connection.commit()
+
+
+def insert_jobs(connection, jobs):
+    for job in jobs:
+        insert_job(connection, job)
+
+
+def get_all_jobs(connection):
+    cursor = connection.cursor()
+    cursor.execute("SELECT * FROM jobs")
+    jobs = cursor.fetchall()
+    return jobs
--- src/main.py
+++ src/main.py
@@ -1,8 +1,18 @@
 from fetcher import fetch_jobs
+from database import connect_database, create_jobs_table ,insert_jobs, get_all_jobs
 
-print(fetch_jobs(
+connection = connect_database()
+create_table = create_jobs_table(connection)
+
+jobs = fetch_jobs(
     keyword="python",
     country="gb",
     results_per_page=5,
     page=1
-))
+)
+
+insert_jobs = insert_jobs(connection, jobs)
+
+all_jobs = get_all_jobs(connection)
+print(len(all_jobs))
+print(all_jobs[0])
